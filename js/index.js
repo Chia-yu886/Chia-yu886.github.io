503f6642c5bb82be78e94cf6c275f5d22a960f26
valerie <jiayuz898@gmail.com>
2024-12-24 00:28:22 +0800

    修復前端顯示問題

--- js/index.js
+++ js/index.js
@@ -106,10 +106,11 @@ async function searchBooks() {
     }
 }
 
+// 搜尋書籍
 function displaySearchResults(books) {
     const resultsContainer = document.getElementById("searchResults");
 
-    // 清空結果容器
+    // 清空現有結果
     resultsContainer.innerHTML = "";
 
     if (books.length === 0) {
@@ -117,43 +118,42 @@ function displaySearchResults(books) {
         return;
     }
 
-    // 遍歷書籍數據並生成對應的 HTML
+    // 遍歷書籍數據並生成 HTML
     books.forEach(book => {
         const bookElement = `
-            <div class="col-12 book-item">
-            <h2>查詢結果</h2>
-                 <div class="row detail">
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>書名：</strong></span>
+            <div class="col-12 book-item border p-3 mb-3 bg-light rounded">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <span>書名：</span>
                         <h5>${book.title}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>作者：</strong></span>
+                    <div class="col-lg-3 col-md-6">
+                        <span>作者：</span>
                         <h5>${book.author}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>書籍狀態：</strong></span>
+                    <div class="col-lg-3 col-md-6">
+                        <span>書籍狀態：</span>
                         <h5>${book.book_condition}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>價格：</strong></span>
+                    <div class="col-lg-3 col-md-6">
+                        <span>價格：</span>
                         <h5>NT$ ${book.price}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>賣家暱稱：</strong></span>
+                    <div class="col-lg-3 col-md-6">
+                        <span>賣家暱稱：</span>
                         <h5>${book.seller_nickname}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>賣家信箱：</strong></span>
+                    <div class="col-lg-3 col-md-6">
+                        <span>賣家信箱：</span>
                         <h5>${book.seller_email}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>賣家系所：</strong></span>
+                    <div class="col-lg-3 col-md-6">
+                        <span>賣家系所：</span>
                         <h5>${book.department}</h5>
                     </div>
-                    <div class="col-lg-6 col-md-12">
-                        <span><strong>販售狀態：</strong></span>
-                        <h5 class="status-${book.status === "可交易" ? "available" : "reserved"}">
+                    <div class="col-lg-3 col-md-6">
+                        <span>販售狀態：</span>
+                        <h5 class="${book.status === '可交易' ? 'text-success' : 'text-danger'}">
                             ${book.status}
                         </h5>
                     </div>
